ada48a0be882b2da2cb63de10995712db62c436d
Freddy Moreno Betancourt <freddy.moreno@xintec.cl>
2021-07-29 00:26:35 -0400

    23. Validación de Entrada

--- src/task/dto/task.dto.ts
+++ src/task/dto/task.dto.ts
@@ -1,4 +1,12 @@
+import { IsBoolean, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class TaskDTO{
-    readonly description:string;
-    readonly isDone:boolean;
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(5)
+    readonly description: string;
+
+    @IsNotEmpty()
+    @IsBoolean()
+    readonly isDone: boolean;
 }
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, Req } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Put,
+    Query,
+    Req,
+    // UsePipes,
+    // ValidationPipe,
+} from '@nestjs/common';
 import { Request } from 'express';
 import { TaskDTO } from './dto/task.dto';
 import { TaskService } from './task.service';
@@ -91,6 +104,7 @@ export class TaskController {
 
     constructor(private readonly taskService:TaskService) {}
     @Post()
+    //@UsePipes(new ValidationPipe())
     create(@Body() taskDTO: TaskDTO)
     {
         return this.taskService.create(taskDTO);
